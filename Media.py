652b94d49244a7f326febcbda4be669e7d8479b0
Yasmin Devegili <48127784+yasmindevegili@users.noreply.github.com>
2022-09-26 20:25:53 -0300

    Update Media.py

--- Media.py
+++ Media.py
@@ -1,9 +1,9 @@
 n = []
 soma = 0
 for i in range(3):
-    n+=[float(input(f"Digite uma nota {i+1}:"))]
-    soma += n[i]
+    nota = float(input(f"Digite uma nota {i+1}:"))
+    n.append(nota)
 
-media = soma/(i+1)
+media = sum(n)/len(n)
 print(f"Media = {media:10.2f}")
     
