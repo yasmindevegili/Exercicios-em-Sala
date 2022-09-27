84e2797e0628303ef30e615bed8bbbc998fe6fcd
Yasmin Devegili <48127784+yasmindevegili@users.noreply.github.com>
2022-09-26 21:36:28 -0300

    Update MaiorMenor.c

--- MaiorMenor.c
+++ MaiorMenor.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 int main(){
-    float n[5];
-    int i, maior, menor;
+    float n[5], maior, menor;
+    int i;
     for(i=0;i<5;i++){
     printf("Digite a nota %i:", i+1);
     scanf("%f", &n[i]);
+    if (i==0){
+        maior = n[i];
+        menor = n[i];
+    }
     if(n[i]>maior){
         maior = n[i];
     }
@@ -12,9 +16,7 @@ int main(){
         menor = n[i];
     }
     }
-    printf("%i %i", maior, menor);
     
-    
-    
+    printf("Maior: %.1f\nMenor: %.1f", maior, menor);
     
 }
